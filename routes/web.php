af77bcb7c5ad1c151162c976f6eee8ec3b5213e1
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2021-08-01 21:19:13 -0500

    Adding required auth & adminLTE template

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
 
 
 
 Auth::routes();
-Route::get('/{any}', [HomeController::class, 'index'])->where('any', '.*');
+Route::get('/{any}', [AppController::class, 'index'])->where('any', '.*');
 
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
